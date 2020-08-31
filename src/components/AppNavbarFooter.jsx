5588f140546cb8d7efb7a0c181c6e349950df52f
saitergun <tiasweb@gmail.com>
2020-08-31 17:06:26 +0300

    update navbar footer classes

--- src/components/AppNavbarFooter.jsx
+++ src/components/AppNavbarFooter.jsx
@@ -11,37 +11,37 @@ const AppNavbarFooter = () => {
   const isHistory = location.pathname === '/history';
 
   return (
-    <span className="navbar-footer-mobile">
-      <nav className="navbar-footer-mobile-menu">
+    <span className="fixed left-0 right-0 bottom-0 z-20 transition-transform duration-500 ease-linear transform translate-y-0">
+      <nav className="flex bg-white shadow-xs px-4 -mb-px -mx-px">
         <NavLink
           to="/"
-          className="menu-item"
-          activeClassName="active"
+          className="relative w-1/3 h-14 flex flex-col items-center justify-center z-10"
+          activeClassName="text-primary-700"
           exact
         >
-          <span className="menu-item-icon">
+          <span className="block w-6 h-6">
             <IconHouse fill={isHome} />
           </span>
         </NavLink>
 
         <NavLink
           to="/saved"
-          className="menu-item"
-          activeClassName="active"
+          className="relative w-1/3 h-14 flex flex-col items-center justify-center z-10"
+          activeClassName="text-primary-700"
           exact
         >
-          <span className="menu-item-icon">
+          <span className="block w-6 h-6">
             <IconBookmarks fill={isSaved} />
           </span>
         </NavLink>
 
         <NavLink
           to="/history"
-          className="menu-item"
-          activeClassName="active"
+          className="relative w-1/3 h-14 flex flex-col items-center justify-center z-10"
+          activeClassName="text-primary-700"
           exact
         >
-          <span className="menu-item-icon">
+          <span className="block w-6 h-6">
             <IconClock fill={isHistory} />
           </span>
         </NavLink>
