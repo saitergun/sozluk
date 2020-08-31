7c698171e6ed4ae8f2e446a40046d22335c1e1ee
saitergun <tiasweb@gmail.com>
2020-08-31 18:49:54 +0300

    adapt Navbar Footer to desktop

--- src/components/AppNavbarFooter.jsx
+++ src/components/AppNavbarFooter.jsx
@@ -12,40 +12,42 @@ const AppNavbarFooter = () => {
 
   return (
     <span className="fixed left-0 right-0 bottom-0 z-20 transition-transform duration-500 ease-linear transform translate-y-0">
-      <nav className="flex bg-white shadow-xs px-4 -mb-px -mx-px">
-        <NavLink
-          to="/"
-          className="relative w-1/3 h-14 flex flex-col items-center justify-center z-10"
-          activeClassName="text-primary-700"
-          exact
-        >
-          <span className="block w-6 h-6">
-            <IconHouse fill={isHome} />
-          </span>
-        </NavLink>
+      <span className="block sm:max-w-lg mx-auto">
+        <nav className="flex bg-white shadow-xs px-4 -mb-px -mx-px">
+          <NavLink
+            to="/"
+            className="relative w-1/3 h-14 flex flex-col items-center justify-center z-10"
+            activeClassName="text-primary-700"
+            exact
+          >
+            <span className="block w-6 h-6">
+              <IconHouse fill={isHome} />
+            </span>
+          </NavLink>
 
-        <NavLink
-          to="/saved"
-          className="relative w-1/3 h-14 flex flex-col items-center justify-center z-10"
-          activeClassName="text-primary-700"
-          exact
-        >
-          <span className="block w-6 h-6">
-            <IconBookmarks fill={isSaved} />
-          </span>
-        </NavLink>
+          <NavLink
+            to="/saved"
+            className="relative w-1/3 h-14 flex flex-col items-center justify-center z-10"
+            activeClassName="text-primary-700"
+            exact
+          >
+            <span className="block w-6 h-6">
+              <IconBookmarks fill={isSaved} />
+            </span>
+          </NavLink>
 
-        <NavLink
-          to="/history"
-          className="relative w-1/3 h-14 flex flex-col items-center justify-center z-10"
-          activeClassName="text-primary-700"
-          exact
-        >
-          <span className="block w-6 h-6">
-            <IconClock fill={isHistory} />
-          </span>
-        </NavLink>
-      </nav>
+          <NavLink
+            to="/history"
+            className="relative w-1/3 h-14 flex flex-col items-center justify-center z-10"
+            activeClassName="text-primary-700"
+            exact
+          >
+            <span className="block w-6 h-6">
+              <IconClock fill={isHistory} />
+            </span>
+          </NavLink>
+        </nav>
+      </span>
     </span>
   );
 };
