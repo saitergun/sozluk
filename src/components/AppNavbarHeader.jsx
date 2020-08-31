22f5aab99158e7f29515ab04f2254998ac89e1f0
saitergun <tiasweb@gmail.com>
2020-08-31 18:49:38 +0300

    adapt Navbar Header to desktop

--- src/components/AppNavbarHeader.jsx
+++ src/components/AppNavbarHeader.jsx
@@ -22,38 +22,40 @@ const AppNavbarHeader = () => {
           'text-white bg-primary z-40'
         ])}
       >
-        {location.pathname !== '/' &&
-          <button
-            className={classnames([
-              'absolute left-2 top-2',
-              'w-8 h-8 flex flex-grow items-center justify-center',
-              'active:bg-primary-600',
-              'rounded-full focus:outline-none p-1',
-            ])}
-            onClick={() => history.goBack()}
-          ><IconArrow left={true} /></button>
-        }
+        <span className="block relative sm:max-w-lg mx-auto">
+          {location.pathname !== '/' &&
+            <button
+              className={classnames([
+                'absolute left-2 top-2',
+                'w-8 h-8 flex flex-grow items-center justify-center',
+                'active:bg-primary-600',
+                'rounded-full focus:outline-none p-1',
+              ])}
+              onClick={() => history.goBack()}
+            ><IconArrow left={true} /></button>
+          }
 
-        {location.pathname !== '/' &&
-          <button
-            className={classnames([
-              'absolute right-2 top-2',
-              'w-8 h-8 flex flex-grow items-center justify-center',
-              'active:bg-primary-600',
-              'rounded-full focus:outline-none p-1',
-            ])}
-            onClick={() => setShowSearchScreen(true)}
-          ><IconSearch /></button>
-        }
+          {location.pathname !== '/' &&
+            <button
+              className={classnames([
+                'absolute right-2 top-2',
+                'w-8 h-8 flex flex-grow items-center justify-center',
+                'active:bg-primary-600',
+                'rounded-full focus:outline-none p-1',
+              ])}
+              onClick={() => setShowSearchScreen(true)}
+            ><IconSearch /></button>
+          }
 
-        {state.app.title &&
-          <span className="w-full h-12 flex items-center justify-center text-center px-12">
-            <h1
-              title={state.app.title}
-              className="block w-full text-xl truncate"
-            >{state.app.title}</h1>
-          </span>
-        }
+          {state.app.title &&
+            <span className="w-full h-12 flex items-center justify-center text-center px-12">
+              <h1
+                title={state.app.title}
+                className="block w-full text-xl truncate"
+              >{state.app.title}</h1>
+            </span>
+          }
+        </span>
       </nav>
 
       {showSearchScreen &&
