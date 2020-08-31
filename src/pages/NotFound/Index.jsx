9dbd5e346fb870d27942782ea3264cd1e3e00e35
saitergun <tiasweb@gmail.com>
2020-08-31 18:48:29 +0300

    adapt notFound page to desktop

--- src/pages/NotFound/Index.jsx
+++ src/pages/NotFound/Index.jsx
@@ -4,7 +4,7 @@ import { IconEmoji } from '../../components/Icon';
 
 const PageNotFound = () => {
   return (
-    <span className="flex flex-col items-center justify-center text-center p-8 my-4">
+    <span className="flex flex-col items-center justify-center text-center sm:max-w-lg mx-auto p-8 my-4">
       <span className="block w-16 h-16"><IconEmoji frown={true} /></span>
 
       <p className="text-xl mt-4">aradığın şey burada yok</p>
