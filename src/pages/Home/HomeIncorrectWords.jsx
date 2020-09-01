e1f23c50165c01d626861974adc4f6e37768b966
saitergun <tiasweb@gmail.com>
2020-09-01 16:42:57 +0300

    extract HomeIncorrectWords comp. from Home page

---
+++ src/pages/Home/HomeIncorrectWords.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+
+import { IconX, IconCheck } from '../../components/Icon';
+
+const HomeIncorrectWords = ({ title, subtitle, words, isColorful }) => {
+  return (
+    <section className="relative bg-white shadow-sm sm:rounded py-2">
+      {(title || subtitle) &&
+        <header className="py-2 px-4">
+          {title &&
+            <h3 className="font-bold text-lg">{title}</h3>
+          }
+
+          {subtitle &&
+            <h6 className="text-sm text-gray-500">{subtitle}</h6>
+          }
+        </header>
+      }
+
+      <main>
+        <ul className="flex flex-col">
+          {words.map((word, index) => {
+            return (
+              <li
+                key={word.id}
+                className={classnames('flex items-center justify-between py-2 px-4', {
+                  'border-t border-gray-200': index > 0,
+                })}
+              >
+                <span
+                  className={classnames('flex items-center space-x-1', {
+                    'text-red-600': isColorful,
+                  })}
+                >
+                  {isColorful &&
+                    <span className="block w-4 h-4"><IconX /></span>
+                  }
+
+                  <span className="block leading-none">{word.incorrect}</span>
+                </span>
+
+                <span
+                  className={classnames('flex items-center space-x-1', {
+                    'text-green-600': isColorful,
+                  })}
+                >
+                  <span className="block leading-none">{word.correct}</span>
+
+                  {isColorful &&
+                    <span className="block w-4 h-4"><IconCheck /></span>
+                  }
+                </span>
+              </li>
+            );
+          })}
+        </ul>
+      </main>
+    </section>
+  );
+}
+
+HomeIncorrectWords.defaultProps = {
+  title: null,
+  subtitle: null,
+  words: null,
+  isColorful: false,
+};
+
+HomeIncorrectWords.propTypes = {
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+  words: PropTypes.array.isRequired,
+  isColorful: PropTypes.bool,
+};
+
+export default HomeIncorrectWords;
--- src/pages/Home/Index.jsx
+++ src/pages/Home/Index.jsx
@@ -1,11 +1,10 @@
 import React, { useState, useLayoutEffect, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import classnames from 'classnames';
 
-import { IconX, IconCheck } from '../../components/Icon';
 import SearchScreen from '../../components/SearchScreen';
 import HomeNavbar from './HomeNavbar';
 import HomeWordOfDay from './HomeWordOfDay';
+import HomeIncorrectWords from './HomeIncorrectWords';
 
 const PageHome = () => {
   const [showSearchScreen, setShowSearchScreen] = useState(false);
@@ -51,68 +50,19 @@ const PageHome = () => {
           }
 
           {home?.syyd?.length > 0 &&
-            <section className="block relative bg-white shadow-sm">
-              <header className="py-3 px-4">
-                <h3 className="font-semibold text-lg leading-none">sıkça yapılan yanlışlara doğrular</h3>
-
-                {home.syyd.some((item) => item.yanliskelime.search(':') > -1 || item.dogrukelime.search(':') > -1) &&
-                  <h6 className="text-xs text-gray-500 leadidng-none mt-1">(:) uzun okunan hecenin gösterilişi</h6>
-                }
-              </header>
-
-              <main className="border-t border-gray-300">
-                <ul className="flex flex-col">
-                  {home.syyd.map((item, index) => {
-                    return (
-                      <li
-                        key={item.id}
-                        className={classnames('flex items-center justify-between py-3 px-4', {
-                          'border-t border-gray-200': index > 0
-                        })}
-                      >
-                        <span className="flex items-center space-x-1 text-red-600">
-                          <span className="block w-4 h-4"><IconX /></span>
-
-                          <span className="block leading-none">{item.yanliskelime}</span>
-                        </span>
-
-                        <span className="flex items-center space-x-1 text-green-600">
-                          <span className="block leading-none">{item.dogrukelime}</span>
-
-                          <span className="block w-4 h-4"><IconCheck /></span>
-                        </span>
-                      </li>
-                    );
-                  })}
-                </ul>
-              </main>
-            </section>
+            <HomeIncorrectWords
+              title="sıkça yapılan yanlışlara doğrular"
+              subtitle={home.syyd.some((item) => item.yanliskelime.search(':') > -1 || item.dogrukelime.search(':') > -1) ? '(:) uzun okunan hecenin gösterilişi' : undefined}
+              words={home.syyd.map((word) => ({ id: word.id, incorrect: word.yanliskelime, correct: word.dogrukelime }))}
+              isColorful={true}
+            />
           }
 
           {home?.karistirma?.length > 0 &&
-            <section className="block relative bg-white shadow-sm">
-              <header className="py-3 px-4">
-                <h3 className="font-semibold text-lg leading-none">sıkça karıştırılan sözler</h3>
-              </header>
-
-              <main className="border-t border-gray-300">
-                <ul className="flex flex-col divide-y divide-gray-200">
-                  {home.karistirma.slice(0, 9).map((item, index) => {
-                    return (
-                      <li
-                        key={item.id}
-                        className={classnames('flex items-center justify-between py-2 px-4', {
-                          'bg-gray-100': index % 2
-                        })}
-                      >
-                        <span>{item.yanlis}</span>
-                        <span>{item.dogru}</span>
-                      </li>
-                    );
-                  })}
-                </ul>
-              </main>
-            </section>
+            <HomeIncorrectWords
+              title="sıkça karıştırılan sözler"
+              words={home.karistirma.slice(0, 9).map((word) => ({ id: word.id, incorrect: word.yanlis, correct: word.dogru }))}
+            />
           }
         </span>
       }
