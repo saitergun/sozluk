780fcd9fcfd8040d9bb46d19f534d2074d27d218
saitergun <tiasweb@gmail.com>
2021-06-11 05:28:35 +0300

    fix

--- src/store/actions/data.js
+++ src/store/actions/data.js
@@ -18,8 +18,8 @@ export const getWordsByQuery = (query) => {
       .replace(new RegExp('[aAÂâ]', 'gi'), '[aAÂâ]')
       .replace(new RegExp('[iİIıîÎ]', 'gi'), '[iİIıîÎ]')
       .replace(new RegExp('[üÜuUÛû]', 'gi'), '[üÜuUÛû]')
-      .replace(new RegExp('[cCÇc]', 'gi'), '[cCÇc]')
-      .replace(new RegExp('[gGĞg]', 'gi'), '[gGĞg]')
+      .replace(new RegExp('[cCÇç]', 'gi'), '[cCÇç]')
+      .replace(new RegExp('[gGĞğ]', 'gi'), '[gGĞğ]')
       .replace(new RegExp('[sSŞş]', 'gi'), '[sSŞş]')
       .replace(new RegExp('[oOÖö]', 'gi'), '[oOÖö]'), 'gi');
 
